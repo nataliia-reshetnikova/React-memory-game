878c016ee818bff81faaeb9edef971e13def3fbd
nataliereshetnikova <ab1748@gmail.com>
2019-07-04 12:39:58 -0400

    Updates

---
+++ precache-manifest.fa615c0ef47bf9305ab4ec7ea1f28838.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb68e9df1e33755bb4189a9a960baac7",
+    "url": "/React-memory-game/index.html"
+  },
+  {
+    "revision": "0b6c6e6f08f9e63b7ec8",
+    "url": "/React-memory-game/static/css/main.5d423d31.chunk.css"
+  },
+  {
+    "revision": "acc80d623c137348ac94",
+    "url": "/React-memory-game/static/js/2.6e4a076c.chunk.js"
+  },
+  {
+    "revision": "0b6c6e6f08f9e63b7ec8",
+    "url": "/React-memory-game/static/js/main.60bc7534.chunk.js"
+  },
+  {
+    "revision": "e24f366a9ed6b257816c",
+    "url": "/React-memory-game/static/js/runtime~main.5c6ae519.js"
+  }
+]);
